264b6fe9ae14c623bce8d164604d71671ac2bfc8
samuel <samuel.tannous174@gmail.com>
2024-07-29 14:00:46 +0300

    yes

--- js/entry.js
+++ js/entry.js
@@ -4,5 +4,5 @@ document.getElementById('form-container').addEventListener('submit', function(ev
     const player2Name = document.getElementById('player2').value;
     localStorage.setItem('player1Name', player1Name);
     localStorage.setItem('player2Name', player2Name);
-    window.location.href = 'index.html';
+    window.location.href = 'game.html';
 });
