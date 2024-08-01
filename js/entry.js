8fc944e2b95a27cb53a420e98803db9587db6f5f
samuel <samuel.tannous174@gmail.com>
2024-08-01 17:06:00 +0300

    yes

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,6 @@
-document.addEventListener('gameInitialized', function() {
-    const player1Name = document.getElementById('player1').value;
-    const player2Name = document.getElementById('player2').value;
-    localStorage.setItem('player1Name', player1Name);
-    localStorage.setItem('player2Name', player2Name);
-})
+// document.addEventListener('gameInitialized', function() {
+//     const player1Name = document.getElementById('player1').value;
+//     const player2Name = document.getElementById('player2').value;
+//     localStorage.setItem('player1Name', player1Name);
+//     localStorage.setItem('player2Name', player2Name);
+// })
